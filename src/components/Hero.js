e1d54e67820eba2939009887fcf400eef02f69e6
theDe-bugger <jaimil.d@icloud.com>
2023-09-18 00:11:15 -0400

    possible break?

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,6 @@ import IconButton from '@mui/material/IconButton';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import Stack from '@mui/material/Stack';
-import Link from '@mui/material/Link';
 import EmailIcon from '@mui/icons-material/Email';
 import PhoneIcon from '@mui/icons-material/Phone';
 
