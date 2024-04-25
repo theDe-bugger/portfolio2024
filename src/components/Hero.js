a15d6621c4a9c4219ff6220f4e744f7422484029
theDe-bugger <jaimil.d@icloud.com>
2024-04-24 23:57:49 -0400

    update landing

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,8 @@ function Hero() {
 
     useEffect(() => {
         const typed = new Typed(el.current, {
-        strings: ["CS @ University of Waterloo", "Computer Vision + Back-End @ Martinrea", "Software Engineer Intern @ Finastra", "Co-Founder, Director of Tech @ Wat Street"], // Strings to display
+        strings: ["CS @ University of Waterloo", "Software Engineer Intern @ Wambi", "Computer Vision + Back-End @ Martinrea", 
+        "Software Engineer Intern @ Finastra", "Co-Founder, Director of Tech @ Wat Street"], // Strings to display
         // Speed settings, try diffrent values untill you get good results
         startDelay: 300,
         typeSpeed: 50,
@@ -28,33 +29,49 @@ function Hero() {
     }, []);
 
     return ( 
-        <div className='App-header'>
-        <main>
-            <h1>
-                Hi there, I'm Jaimil Dalwadi     <span class="wave">👋🏼</span>
-            </h1>
-            <h4>
-                <span ref={el}></span>
-            </h4>
-            
-            <Stack direction="row" spacing={1} >
-                <IconButton aria-label="mail" sx={{ color: '#0BECE5' }} size = "large" href="mailto:jaimil.dalwadi@uwaterloo.ca" target="_blank">
-                    <EmailIcon />
-                </IconButton>
+        <div className='App-header' >
+        <main className="grid-container">
 
-                <IconButton aria-label="call" sx={{ color: '#0BECE5' }} size = "large" href="tel:+16479820821" target="_blank">
-                    <PhoneIcon />
-                </IconButton>
-
-                <IconButton aria-label="github" sx={{ color: '#0BECE5' }} size = "large" href="https://github.com/theDe-bugger/" target="_blank">
-                    <GitHubIcon />
-                </IconButton>
-
-                <IconButton aria-label="linkedin" sx={{ color: '#0BECE5' }} size = "large" href="https://linkedin.com/in/jaimil-dalwadi" target="_blank">
-                    <LinkedInIcon />
-                </IconButton>
+           
+            <div className ='App-info'>
+                <p style={{fontSize:64}}>
+                <span class="wave">👋🏼</span> 
+                </p>
                 
-            </Stack>
+                <h1>
+                Hi there, I'm Jaimil Dalwadi     
+                </h1>
+                <h4>
+                    <span ref={el}></span>
+                </h4>
+                
+                <Stack direction="row" spacing={1} justifyContent="right" alignItems='right' >
+                    <IconButton aria-label="mail" sx={{ color: '#0BECE5' }} size = "large" href="mailto:jaimil.dalwadi@uwaterloo.ca" target="_blank">
+                        <EmailIcon />
+                    </IconButton>
+
+                    <IconButton aria-label="call" sx={{ color: '#0BECE5' }} size = "large" href="tel:+16479820821" target="_blank">
+                        <PhoneIcon />
+                    </IconButton>
+
+                    <IconButton aria-label="github" sx={{ color: '#0BECE5' }} size = "large" href="https://github.com/theDe-bugger/" target="_blank">
+                        <GitHubIcon />
+                    </IconButton>
+
+                    <IconButton aria-label="linkedin" sx={{ color: '#0BECE5' }} size = "large" href="https://linkedin.com/in/jaimil-dalwadi" target="_blank">
+                        <LinkedInIcon />
+                    </IconButton>
+                    
+                </Stack>
+            </div>
+
+            <div className='App-face'>
+                <img 
+                src={process.env.PUBLIC_URL + "/img/portrait.png"}
+                alt="Jaimil" />
+            </div>
+           
+            
         </main>
         </div>    
     );
